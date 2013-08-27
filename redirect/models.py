4f58608233b79a31e72075e5b0ce49af025d981c
tdphillips <troy@directemployersfoundation.org>
2013-08-27 11:06:27 -0400

    Add model docs [MJ-531]

--- redirect/models.py
+++ redirect/models.py
@@ -9,15 +9,27 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Redirect(models.Model):
-    guid = models.CharField(max_length=32, unique=True)
+    """
+    Contains most of the information required to determine how a url
+    is to be transformed
+    """
+    guid = models.CharField(max_length=32, unique=True,
+                            help_text=_('32-character hex string'))
     buid = models.IntegerField(default=0)
     uid = models.IntegerField(unique=True)
-    url = models.URLField()
-    new_date = models.DateTimeField()
-    expired_date = models.DateTimeField(blank=True, null=True)
+    url = models.URLField(help_text=_('URL being manipulated'))
+    new_date = models.DateTimeField(help_text=_('Date that this job was '
+                                                'added'))
+    expired_date = models.DateTimeField(blank=True, null=True,
+                                        help_text=_('Date that this job was'
+                                                    'marked as expired'))
 
 
 class ATSSourceCode(models.Model):
+    """
+    Represents one entry in a query string of the form
+    ?parameter_name=parameter_value
+    """
     ats_name = models.CharField(max_length=255)
     parameter_name = models.CharField(max_length=255)
     parameter_value = models.CharField(max_length=255)
@@ -29,6 +41,10 @@ class CanonicalMicrosite(models.Model):
 
 
 class RedirectAction(models.Model):
+    """
+    Determines what transformation(s) should take place based on the provided
+    parameters
+    """
     buid = models.IntegerField(default=0)
     view_source = models.ForeignKey('ViewSource')
     action = models.CharField(max_length=255)
@@ -44,6 +60,8 @@ class ViewSource(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.viewsource_id:
+            # if viewsource_id was not provided, set it to the next
+            # available value
             try:
                 latest = ViewSource.objects.values_list('viewsource_id',
                                                         flat=True).latest()
