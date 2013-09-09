b2617c2d5a7a29cfc39ccfa95dc4892437516bb7
tdphillips <troy@directemployersfoundation.org>
2013-09-09 11:48:19 -0400

    All other actions implemented [MJ-562]

--- redirect/helpers.py
+++ redirect/helpers.py
@@ -1,4 +1,5 @@
 import sys
+import urllib
 
 from redirect.models import *
 
@@ -50,3 +51,95 @@ def sourcecodetag(redirect_obj, manipulation_obj):
     url = redirect_obj.url
     url += '%s' % manipulation_obj.Value1
     return url
+
+
+def doubleclickwrap(redirect_obj, manipulation_obj):
+    return manipulation_obj.Value1 + redirect_obj.url
+
+
+def doubleclickunwind(redirect_obj, manipulation_obj):
+    url = redirect_obj.url.split('?')
+    return url[0]
+
+
+def anchorredirectissue(redirect_obj, manipulation_obj):
+    url = redirect_obj.url.split('#')
+    return url[0]
+
+
+def sourcecodeswitch(redirect_obj, manipulation_obj):
+    return redirect_obj.url.replace(manipulation_obj.Value1,
+                                    manipulation_obj.Value2)
+
+
+def sourcecodeinsertion(redirect_obj, manipulation_obj):
+    url = redirect_obj.url.split('#')
+    url = ('%s#' % manipulation_obj.Value1).join(url)
+    return url
+
+
+def sourceurlwrap(redirect_obj, manipulation_obj):
+    url = urllib.urlencode(redirect_obj.url)
+    return manipulation_obj.Value1 + url
+
+
+def sourceurlwrapappend(redirect_obj, manipulation_obj):
+    url = urllib.urlencode(redirect_obj.url)
+    return manipulation_obj.Value1 + url + manipulation_obj.Value2
+
+
+def sourceurlwrapunencoded(redirect_obj, manipulation_obj, value1=None):
+    value1 = value1 or manipulation_obj.Value1
+    return value1 + redirect_obj.url
+    return manipulation_obj.Value1 + redirect_obj.url
+
+
+def sourceurlwrapunencodedappend(redirect_obj, manipulation_obj):
+    url = sourceurlwrapunencoded(redirect_obj,
+                                 manipulation_obj,
+                                 manipulation_obj.Value1)
+    return url + manipulation_obj.Value2
+
+
+def urlswap(redirect_obj, manipulation_obj):
+    return manipulation_obj.Value1
+
+
+def amptoamp(redirect_obj, manipulation_obj):
+    url = redirect_obj.url.split('&')
+    return redirect_obj.Value1 + url[0] + redirect_obj.Value2
+
+
+def switchlastinstance(redirect_obj, manipulation_obj, old=None, new=None):
+    old = value1 or manipulation_obj.Value1
+    new = value2 or manipulation_obj.Value2
+    return new.join(redirect_obj.url.rsplit(old, 1))
+
+
+def switchlastthenadd(redirect_obj, manipulation_obj):
+    old, new = manipulation_obj.Value1.split('!!!!')
+    new_url = switchlastinstance(redirect_obj, manipulation_obj, old, new)
+    return new_url + manipulation_obj.Value2
+
+
+def replace(redirect_obj, manipulation_obj):
+    """
+    Utility function that is used in the `replacethenadd*` actions
+    """
+    old, new = manipulation_obj.Value1.split('!!!!')
+    return redirect_obj.url.replace(old, new)
+
+
+def replacethenadd(redirect_obj, manipulation_obj):
+    url = replace(redirect_obj, manipulation_obj)
+    return url + manipulation_obj.Value2
+
+
+def replacethenaddpre(redirect_obj, manipulation_obj):
+    url = replace(redirect_obj, manipulation_obj)
+    return manipulation_obj.Value2 + url
+
+def cframe(redirect_obj, manipulation_obj):
+    url = urllib.urlencode(redirect_obj.url)
+    url = '%s?url=%s' % (manipulation_obj.Value1, url)
+    return 'http://directemployers.us.jobs/companyframe/' + url
