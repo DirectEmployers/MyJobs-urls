4927f67147e3948abc430d4579d09eb148afea0e
almathew <ashley@directemployersfoundation.org>
2013-10-11 14:32:27 -0400

    Only need localhost in allowed hosts

--- deploy/settings.prod.py
+++ deploy/settings.prod.py
@@ -17,7 +17,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'jcnlx.com', ]
+ALLOWED_HOSTS = ['localhost', ]
 
 # Uncomment for Django Debug Toolbar
 #MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
