c5a7e93ea89d3a804111f59c1b2e66cb080a2456
tdphillips <troy@directemployersfoundation.org>
2013-10-31 13:05:58 -0400

    Add default redirect for guids that have no associated DestinationManipulation [MJ-489]

--- redirect/views.py
+++ redirect/views.py
@@ -13,6 +13,7 @@ def home(request, guid, vsid='0'):
     guid_redirect = get_object_or_404(models.Redirect,
                                       guid='{%s}' % uuid.UUID(guid))
 
+    manipulation = None
     try:
         manipulation = models.DestinationManipulation.objects.get(
             buid=guid_redirect.buid, view_source=vsid, action_type=1)
@@ -21,7 +22,7 @@ def home(request, guid, vsid='0'):
             manipulation = models.DestinationManipulation.objects.get(
                 buid=guid_redirect.buid, view_source=vsid)
         except models.DestinationManipulation.DoesNotExist:
-            raise Http404
+            pass
 
     expired = False
     facebook = False
@@ -36,15 +37,15 @@ def home(request, guid, vsid='0'):
     
     # open graph bot redirect
     if 'facebookexternalhit' in str(user_agent):
-        user_agent_vs = 1593
+        user_agent_vs = '1593'
         redirect_user_agent = True
     
     if 'twitterbot' in str(user_agent):
-        user_agent_vs = 1596
+        user_agent_vs = '1596'
         redirect_user_agent = True
         
     if 'linkedinbot' in str(user_agent):
-        user_agent_vs = 1548
+        user_agent_vs = '1548'
         redirect_user_agent = True
     
     if redirect_user_agent:
@@ -73,7 +74,7 @@ def home(request, guid, vsid='0'):
         html += "</html>"        
         response = HttpResponse(content=html, mimetype='text/html')
     else:    
-        if manipulation.view_source == 1604:
+        if vsid == '1604':
             # msccn redirect
     
             company_name = guid_redirect.company_name
@@ -81,10 +82,10 @@ def home(request, guid, vsid='0'):
             redirect_url = ('http://us.jobs/msccn-referral.asp?gi='
                             '%s%s&cp=%s&u=%s' %
                             (clean_guid,
-                             manipulation.view_source,
+                             vsid,
                              company_name,
                              guid_redirect.uid))
-        elif manipulation.view_source == 294:
+        elif vsid == '294':
             # facebook redirect
             facebook = True
     
@@ -92,19 +93,22 @@ def home(request, guid, vsid='0'):
                 expired = True
     
             redirect_url = 'http://apps.facebook.com/us-jobs/?jvid=%s%s' % \
-                (clean_guid, manipulation.view_source)
+                (clean_guid, vsid)
         else:
             if guid_redirect.expired_date:
                 expired = True
+
+            if manipulation:
+                method_name = manipulation.action
+
+                try:
+                    redirect_method = getattr(helpers, method_name)
+                except AttributeError:
+                    pass
     
-            method_name = manipulation.action
-    
-            try:
-                redirect_method = getattr(helpers, method_name)
-            except AttributeError:
-                pass
-    
-            redirect_url = redirect_method(guid_redirect, manipulation)
+                redirect_url = redirect_method(guid_redirect, manipulation)
+            else:
+                redirect_url = guid_redirect.url
     
         aguid = request.COOKIES.get('aguid') or \
             helpers.quote_string('{%s}' % str(uuid.uuid4()))
