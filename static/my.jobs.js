341e04673b9b26557a56c5302510133e406630fe
tdphillips <troy@directemployersfoundation.org>
2014-01-29 14:14:18 -0500

    Improve expired jobs design and style [RD-68]
    Start using LESS

--- static/my.jobs.js
+++ static/my.jobs.js
@@ -32,8 +32,13 @@ function populate_toolbar(data) {
         nav_html += '</ul>';
         nav_html += '</li>';
 
+        console.log(user_gravatar);
         $("#nav").html(nav_html);
         $("#nav").removeClass("menu-login");
+        if ($('.main-nav img').length) {
+            $("#nav").addClass("gravatar");
+            $('.main-nav img').addClass('gravatar-info');
+        }
      }
      $("#account").removeClass("hidden");
 }
