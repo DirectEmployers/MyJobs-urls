4679bb51217c824c7ba4dcffb4aa0119f9bd44f4
tdphillips <troy@directemployersfoundation.org>
2014-06-05 11:28:06 -0400

    Finish comment

--- redirect/tests/views.py
+++ redirect/tests/views.py
@@ -762,7 +762,8 @@ class ViewSourceViewTests(TestCase):
                       'vs': ['20']}.items():
             self.assertEqual(query[param[0]], param[1])
 
-        #
+        # When redirecting from a feed and using custom parameters, we create
+        # the potential for a redirect loop. Ensure that isn't happening.
         self.assertRaises(KeyError, lambda: query['my.jobs.site.id'])
 
     def test_source_codes_with_hit_key(self):
